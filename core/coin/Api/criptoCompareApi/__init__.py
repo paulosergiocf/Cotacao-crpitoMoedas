1279813fdb7b1ae1d284f86a106ae45c8962b137
Paulo Sergio de Campos Filho <paulosergiocf.dev@gmail.com>
2022-08-16 21:48:29 -0300

    Update: Organizada as classes da api

---
+++ core/coin/Api/criptoCompareApi/__init__.py
@@ -0,0 +1,14 @@
+import requests
+
+class CriptoCompare():
+    def __init__(self, keyApi):
+        self.keyApi = keyApi
+        
+    def cotarPorMoeda(self, fsym, tsyms):
+        requestHttp = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={fsym}&tsyms={tsyms}&api_key={self.keyApi}").json()
+        return requestHttp
+
+    def cotarGeral(self, tsyms):
+        # criar forma de pesquisar geral
+        pass
+        # return requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={fsym}&tsyms={tsyms}&api_key={self.keyApi}")
