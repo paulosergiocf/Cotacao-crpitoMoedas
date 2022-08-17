ba126e67246b269c5a2c87bc878dab87d3a537f9
Paulo Sergio de Campos Filho <paulosergiocf.dev@gmail.com>
2022-08-16 21:50:41 -0300

    Update: regularizado a questão das chamdas na api e o enviou para pagina inicial

--- core/coin/views.py
+++ core/coin/views.py
@@ -2,16 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Cotacoes
 
-from coin.criptoCompare import CriptoCompare
-
+from coin.Api.criptoCompareApi import CriptoCompare
 # Create your views here.
 
 cc = CriptoCompare("e2fd112ffaf6388810cb1d315ad76a3093d2a28e1a7b0135060f30ef89207b1f")
  
 def index(request):
-    
+   
     dados = {
-        'btc': cc.cotar("BTC", "USD")
+        "cotacoes": 
+            [
+                {"BitCoin":cc.cotarPorMoeda("BTC", "BRL"), 
+                "Etherium":cc.cotarPorMoeda("ETH", "BRL"),
+                "BnB":cc.cotarPorMoeda("BNB", "BRL"),
+                "XRP":cc.cotarPorMoeda("XRP", "BRL")}],
     }
-    return render(request,'index.html', dados)
+
+    return render(request,'index.html',dados)
   
