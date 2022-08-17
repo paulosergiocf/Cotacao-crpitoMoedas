394e345eae125b2d14e351cba726c91e224dac0f
Paulo Sergio de Campos Filho <paulosergiocf.dev@gmail.com>
2022-08-17 19:36:30 -0300

    Update para pode fazer besteira

---
+++ core/coin/BdSqlite3/__init__.py
@@ -0,0 +1,34 @@
+import sqlite3
+from datetime import datetime 
+
+
+class BancoDeDados:
+    def __init__(self):
+        self.conexao = sqlite3.connect(r'core/db1.sqlite3')
+        self.criarTabela()
+    
+    def criarTabela(self):
+        try:
+            c = self.conexao.cursor()
+            c.execute("""create table if not exists coin_cotacoes (
+                        id integer primary key autoincrement,
+                        coin varchar(200),
+                        valor real,
+                        dataCoin datetime)""")
+
+            self.conexao.commit()
+            c.close()
+    
+        except Exception as erro:
+            print(F'!BANCO DE DADOS: {erro}')
+            
+    def insertDados(self, coin: str, valor: float):
+        try:
+            c = self.conexao.cursor()
+            c.execute('INSERT INTO coin_cotacoes (coin, valor, dataCoin) VALUES (?, ?, ?)', (coin, valor, datetime.now()))
+            self.conexao.commit()
+            c.close()
+
+
+        except Exception as erro:
+            raise erro
