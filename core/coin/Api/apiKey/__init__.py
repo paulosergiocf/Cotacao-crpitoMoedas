3bef9c69a15195050cd9e9efafaba9b5401c1aff
Paulo Sergio de Campos Filho <paulosergiocf.dev@gmail.com>
2022-08-16 22:08:38 -0300

    update: sepração da chave do código

---
+++ core/coin/Api/apiKey/__init__.py
@@ -0,0 +1,21 @@
+
+class AcessoApi:
+    def __init__(self, path):
+        self.__key = ''
+        self.__path = path
+        
+        self.importKey()
+        
+    @property
+    def key(self):
+        return self.__key
+
+    def importKey(self):
+        try:
+            fileKey = open(self.__path)
+            self.__key = fileKey.readline()
+            fileKey.close()
+        except Exception as erro:
+            raise erro
+
+        
--- core/coin/views.py
+++ core/coin/views.py
@@ -3,19 +3,39 @@ from django.shortcuts import render
 from .models import Cotacoes
 
 from coin.Api.criptoCompareApi import CriptoCompare
+from coin.Api.apiKey import AcessoApi
 # Create your views here.
-
-cc = CriptoCompare("e2fd112ffaf6388810cb1d315ad76a3093d2a28e1a7b0135060f30ef89207b1f")
+api = AcessoApi('apiKey.key')
+cc = CriptoCompare(api.key)
  
 def index(request):
    
     dados = {
         "cotacoes": 
             [
-                {"BitCoin":cc.cotarPorMoeda("BTC", "BRL"), 
-                "Etherium":cc.cotarPorMoeda("ETH", "BRL"),
-                "BnB":cc.cotarPorMoeda("BNB", "BRL"),
-                "XRP":cc.cotarPorMoeda("XRP", "BRL")}],
+                {
+                    "BitCoin":cc.cotarPorMoeda("BTC", "BRL"), 
+                    "Etherium":cc.cotarPorMoeda("ETH", "BRL"),
+                    "BnB":cc.cotarPorMoeda("BNB", "BRL"),
+                    "XRP":cc.cotarPorMoeda("XRP", "BRL"),
+                    "USDT":cc.cotarPorMoeda("USDT", "BRL"),
+                    "USDC":cc.cotarPorMoeda("USDC", "BRL"),
+                    "ADA":cc.cotarPorMoeda("ADA", "BRL"),
+                    "BUSD":cc.cotarPorMoeda("BUSD", "BRL"),
+                    "SOL":cc.cotarPorMoeda("SOL", "BRL"),
+                    "DOGE":cc.cotarPorMoeda("DOGE", "BRL"),
+                    "DOT":cc.cotarPorMoeda("DOT", "BRL"),
+                    "SHIB":cc.cotarPorMoeda("SHIB", "BRL"),
+                    "XMR":cc.cotarPorMoeda("XMR", "BRL"),
+                    "LTC":cc.cotarPorMoeda("LTC", "BRL"),
+                    "CHZ":cc.cotarPorMoeda("CHZ", "BRL"),
+                    "XLM":cc.cotarPorMoeda("XLM", "BRL"),
+                    "VET":cc.cotarPorMoeda("VET", "BRL"),
+                    "FIL":cc.cotarPorMoeda("FIL", "BRL"),
+                    "ATOM":cc.cotarPorMoeda("ATOM", "BRL")
+                
+                }
+                ],
     }
 
     return render(request,'index.html',dados)
